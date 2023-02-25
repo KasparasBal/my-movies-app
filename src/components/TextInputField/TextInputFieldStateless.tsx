eb6cd2a8c51494dd261bc955a7732218e90c7fbb
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2023-02-25 10:56:24 +0200

    Added Text input and select input

---
+++ src/components/TextInputField/TextInputFieldStateless.tsx
@@ -0,0 +1,12 @@
+import styles from './TextInputFieldStateless.module.css';
+
+type Props = {
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const TextInputFieldStateless: React.FC<Props> = ({ placeholder, value, onChange }: Props) => {
+  return <input className={styles.filterInput} placeholder={placeholder} type="text" value={value} onChange={onChange}></input>;
+};
+export default TextInputFieldStateless;
